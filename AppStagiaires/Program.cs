3605a1fcaba362b3b952879ccf9af25d05fa7175
anaslgh <anaslgh@gmail.com>
2015-01-23 23:44:37 +0000

    coomit tp05

---
+++ AppStagiaires/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace ExecutionRequeteInsert{
+    class Program{
+        static void Main(string[] args){
+
+            string connexionString = @"Data Source=hp-pc\sqlexpress;Initial Catalog= GestionStagaires;Integrated Security =true";
+
+            SqlConnection connexion = new SqlConnection(connexionString);
+            SqlCommand commande = connexion.CreateCommand();
+            commande.CommandText = "Insert into Stagaires(Id,Nom) values(8,'Madani')";
+            connexion.Open();
+            commande.ExecuteNonQuery();
+            connexion.Close();
+        }
+    }
+}
